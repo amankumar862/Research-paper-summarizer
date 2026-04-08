542db4ed3f1896b78dae5212e81ec1999aaa2a26
amankumar862 <amankumar86227@gmail.com>
2026-04-08 11:42:57 +0530

    Delete Research Paper Summarizer directory

--- Research Paper Summarizer/static/js/script.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector("form");
-const btn = document.getElementById("btn");
-const loader = document.getElementById("loader");
-
-form.addEventListener("submit", () => {
-    btn.innerText = "Generating...";
-    btn.disabled = true;
-    loader.style.display = "block";
-});
